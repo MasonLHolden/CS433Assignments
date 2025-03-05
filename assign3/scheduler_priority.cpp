201245bf00a022cf7134d393425046da0d4d33fa
Judah-F <Judahfisher21@gmail.com>
2025-03-04 19:25:30 -0800

    Update scheduler_priority.cpp

--- assign3/scheduler_priority.cpp
+++ assign3/scheduler_priority.cpp
@@ -1,7 +1,7 @@
 /**
 * Assignment 3: CPU Scheduler
  * @file scheduler_priority.cpp
- * @author ??? (TODO: your name)
+ * @author Mason Lavender Holden, Judah Fisher
  * @brief This Scheduler class implements the Priority scheduling algorithm.
  * @version 0.1
  */
