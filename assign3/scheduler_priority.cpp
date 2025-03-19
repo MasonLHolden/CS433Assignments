a4eb676bc3e42d022ebabcf11301b3683592b4d7
Judah-F <Judahfisher21@gmail.com>
2025-03-18 19:54:41 -0700

    Update scheduler_priority.cpp
    Fixed mixup with Priority RR

--- assign3/scheduler_priority.cpp
+++ assign3/scheduler_priority.cpp
@@ -1,32 +1,35 @@
 /**
 * Assignment 3: CPU Scheduler
- * @file scheduler_priority_rr.cpp
- * @author Mason Lavender Holden, Judah Fisher
- * @brief This Scheduler class implements the Priority RR scheduling algorithm.
+ * @file scheduler_priority.cpp
+ * @author Mason Lavender Holden
+ * @brief This Scheduler class implements the Priority scheduling algorithm.
  * @version 0.1
  */
 //You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
 // Remember to add sufficient and clear comments to your code
 
-#include "scheduler_priority_rr.h"
 
-// TODO: add implementation of SchedulerPriorityRR constructor, destrcutor and 
+#include "scheduler_priority.h"
+
+// TODO: add implementation of SchedulerPriority constructor, destrcutor and
 // member functions init, print_results, and simulate here
-void init(std::vector<PCB>& process_list){
-
-    processes=process_list;
 
 
-}
-
-
-void print_results(){
+SchedulerPriority:: SchedulerPriority() {
 
 }
 
-/**
- * @brief This function simulates the scheduling of processes in the ready queue.
- *        It stops when all processes are finished.
- */
-void simulate() {
+SchedulerPriority::~SchedulerPriority() {
+
+}
+void SchedulerPriority:: init(std::vector<PCB>& process_list) {
+
+}
+
+void SchedulerPriority:: print_results() {
+
+}
+
+void SchedulerPriority:: simulate() {
+
 }
