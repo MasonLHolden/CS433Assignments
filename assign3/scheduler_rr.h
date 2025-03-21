e19f853d234846c4516e7c10f9dea27bb5acc619
Judah-F <Judahfisher21@gmail.com>
2025-03-21 09:01:19 -0700

    Update scheduler_rr.h

--- assign3/scheduler_rr.h
+++ assign3/scheduler_rr.h
@@ -7,63 +7,52 @@
  */
 //You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
 // Remember to add sufficient and clear comments to your code
-//
 
-#include "scheduler_rr.h"
+#ifndef ASSIGN3_SCHEDULER_RR_H
+#define ASSIGN3_SCHEDULER_RR_H
 
-// TODO: add implementation of SchedulerRR constructor, destrcutor and
-// member functions init, print_results, and simulate here
-SchedulerRR:: SchedulerRR(int time_quantum) {
+#include "scheduler.h"
 
-}
-SchedulerRR::~SchedulerRR() {
-
-}
-
-void SchedulerRR:: init(std::vector<PCB>& process_list){
-
-    //copy of list
-    processes = process_list;
-
-    //reset sim time
-    current_time = 0;
-
-    //allocate space
-    turnaround_times.resize(processes.size());
-    waiting_times.resize(processes.size());
-
-    //sort?
-    //sort(processes.begin(), processes.end(),[](const PCB& a, const PCB& b){return a.arrival_time < b.arrival_time;});
-  }
-
-
-void SchedulerRR:: print_results(){
-
-  }
-
-/**
- * @brief This function simulates the scheduling of processes in the ready queue.
- *        It stops when all processes are finished.
- */
-void SchedulerRR:: simulate() {
-    /*
-
-    for i < processes size //outer loop
-    {
-    while(processes[i] burst Time != 0)
-     if(burst_time < quantum)
-     {
-        current_time = current_time + burst_time
-        burst_time = 0;
-     }
-
-    else if(burst_time >= quantum)
-        {
-            burst_time=burst_time - quantum
-            current_time = current_time + burst_time
-        }
-
-        }
+class SchedulerRR : public Scheduler {
+private:
+    // TODO: add necessary member variables here for your implementation
+    float current_time;
+    vector<float>turnaround_times;
+    vector<float>waiting_times;
+    vector<PCB> processes;
+    float avg_turnaround;
+    float avg_waiting;
+    int quantum;
+public:
+    /**
+     * @brief Construct a new SchedulerRR object
      */
+    SchedulerRR(int time_quantum = 10);
 
-  }
+    /**
+     * @brief Destroy the SchedulerRR object
+     */
+    ~SchedulerRR() override;
+
+    /**
+     * @brief This function is called once before the simulation starts.
+     *        It is used to initialize the scheduler.
+     * @param process_list The list of processes in the simulation.
+     */
+    void init(std::vector<PCB>& process_list) override;
+
+    /**
+     * @brief This function is called once after the simulation ends.
+     *        It is used to print out the results of the simulation.
+     */
+    void print_results() override;
+
+    /**
+     * @brief This function simulates the scheduling of processes in the ready queue.
+     *        It stops when all processes are finished.
+     */
+    void simulate() override;
+};
+
+
+#endif //ASSIGN3_SCHEDULER_RR_H
