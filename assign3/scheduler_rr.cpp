859e5e577d3263b8e7f023cf582d6e11c7d2818e
Judah-F <Judahfisher21@gmail.com>
2025-03-21 09:00:16 -0700

    Update scheduler_rr.cpp

--- assign3/scheduler_rr.cpp
+++ assign3/scheduler_rr.cpp
@@ -22,13 +22,49 @@ SchedulerRR::~SchedulerRR() {
 
 void SchedulerRR:: init(std::vector<PCB>& process_list){
 
-//  processes=process_list;
+    //copy of list
+    processes = process_list;
 
+    //reset sim time
+    current_time = 0;
 
+    //allocate space
+    turnaround_times.resize(processes.size());
+    waiting_times.resize(processes.size());
+
+    //sort?
+    //sort(processes.begin(), processes.end(),[](const PCB& a, const PCB& b){return a.arrival_time < b.arrival_time;});
   }
 
 
 void SchedulerRR:: print_results(){
+
+  }
+
+/**
+ * @brief This function simulates the scheduling of processes in the ready queue.
+ *        It stops when all processes are finished.
+ */
+void SchedulerRR:: simulate() {
+    /*
+
+    for i < processes size //outer loop
+    {
+    while(processes[i] burst Time != 0)
+     if(burst_time < quantum)
+     {
+        current_time = current_time + burst_time
+        burst_time = 0;
+     }
+
+    else if(burst_time >= quantum)
+        {
+            burst_time=burst_time - quantum
+            current_time = current_time + burst_time
+        }
+
+        }
+     */
 
   }
 
