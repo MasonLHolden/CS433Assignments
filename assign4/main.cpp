1971d2b2b4757580cd13ea474cc0193e9e2f1d5b
Judah-F <Judahfisher21@gmail.com>
2025-03-26 10:09:06 -0700

    Update main.cpp
    names

--- assign4/main.cpp
+++ assign4/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Assignment 4: Producer Consumer Problem
  * @file main.cpp
- * @author ??? (TODO: your name)
+ * @author Mason Holden, Judah Fisher
  * @brief The main program for the producer consumer problem.
  * @version 0.1
  */
