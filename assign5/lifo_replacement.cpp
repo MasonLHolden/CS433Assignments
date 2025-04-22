8ac2808d6a6b22484a41bed6126f4c46bdbe1f62
Judah-F <Judahfisher21@gmail.com>
2025-04-22 13:35:42 -0700

    Update lifo_replacement.cpp

--- assign5/lifo_replacement.cpp
+++ assign5/lifo_replacement.cpp
@@ -1,7 +1,7 @@
 /**
 * Assignment 5: Page replacement algorithms
  * @file lifo_replacement.cpp
- * @author ??? (TODO: your name)
+  * @author Mason Lavender Holden, Judah Fisher
  * @brief A class implementing the Last in First Out (LIFO) page replacement algorithms
  * @version 0.1
  */
