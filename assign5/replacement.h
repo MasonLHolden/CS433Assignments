4a41d54608b472716a3bc51adfe3e2651c0891a8
Judah-F <Judahfisher21@gmail.com>
2025-04-22 13:38:06 -0700

    Update replacement.h

--- assign5/replacement.h
+++ assign5/replacement.h
@@ -1,7 +1,7 @@
 /**
 * Assignment 5: Page replacement algorithms
  * @file replacement.h
- * @author ??? (TODO: your name)
+ *  * @author Mason Lavender Holden, Judah Fisher
  * @brief A base class for different page replacement algorithms.
  * @version 0.1
  */
